1c091268ab1719520b83e2dd9587a28058fcdc3a
Gustave Anthony Dzoumedor <dzoumedor13@gmail.com>
2021-11-19 01:27:02 +0000

    Added languages and minor fixs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+
 import PinnedRepo from "./Screens/PinnedRepo";
 import "./App.css";
 
 function App() {
+  // const Github Repo
+  // language;
+  const [gitRepos, getRepo] = useState([]);
+  useEffect(() => {
+    Axios.get("https://api.github.com/users/vanderkilu/repos?per_page=15" + "&client_id=" + "be0d2752ae8c6d86f1c8" + "&client_secret=" + "7404aa5474a35d1de9999d06cb72bfcaa958d9b4").then((response) => {
+      //filter out repos who dont have a language and description
+      let newRepo = [];
+      (response.data || []).map((res) => {
+        if (res.description && res.language && res.stargazers_count >= 0) {
+          return newRepo.push(res);
+        }
+      });
+      getRepo(newRepo);
+    });
+  }, []);
+
   return (
     <div className="container">
       <div className="row pt-5 mt-5">
@@ -9,11 +28,19 @@ function App() {
           {" "}
           <side className="profile bg-dark">
             {" "}
-            <h1>siderbar</h1>{" "}
+            <h1>sidebar</h1>{" "}
           </side>
         </div>
         <div className="col-9">
-          <PinnedRepo />
+          <div className="d-flex flex-row justify-content-between align-items-center">
+            <p className="h6">Pinned</p>
+            <p className="customize text-muted text-decoration-none">Customize your pins</p>
+          </div>
+          <div className="row">
+            {gitRepos.map((repo) => {
+              return <PinnedRepo repo={repo} key={repo.id} />;
+            })}
+          </div>
         </div>
       </div>
     </div>
--- src/Screens/PinnedRepo.js
+++ src/Screens/PinnedRepo.js
@@ -1,103 +1,39 @@
-import React, { useEffect } from "react";
+function PinnedRepo({ repo }) {
+  // });
 
-function PinnedRepo() {
   return (
     <>
-      <div className="d-flex flex-row justify-content-between align-items-center">
-        <p className="h6">Pinned</p>
-        <p className="h6">Customize your pins</p>
-      </div>
-      <div className="row">
-        <div className="col-6 pb-4 mb-2">
-          <div className="border rounded p-3 pinned-container">
-            <div className="d-flex flex-row justify-content-between ">
-              <a href="#" className="h6">
-                project title
+      <div className="col-6 ">
+        <div className="card border rounded pinned-container">
+          <div className="card-body position-relative">
+            <div className="card-title d-flex flex-row justify-content-between ">
+              <a href={`${repo.svn_url}`} target="_blank" rel="noreferrer" className=" text-decoration-none title">
+                {repo.name}{" "}
               </a>
               <span className="h6 text-muted text-small">
-                <small className="border rounded-pill p-1">Public </small>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-grid-3x3-gap mx-2" viewBox="0 0 16 16">
-                  <path d="M4 2v2H2V2h2zm1 12v-2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V7a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm5 10v-2a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V7a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V2a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zM9 2v2H7V2h2zm5 0v2h-2V2h2zM4 7v2H2V7h2zm5 0v2H7V7h2zm5 0h-2v2h2V7zM4 12v2H2v-2h2zm5 0v2H7v-2h2zm5 0v2h-2v-2h2zM12 1a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1h-2zm-1 6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V7zm1 4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1h-2z" />
-                </svg>
-              </span>{" "}
-            </div>
-            <div>
-              <p className="text-truncate text-small text-muted"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos optio sequi veritatis, velit quisquam, debitis similique itaque deleniti, adipisci eius corrupti eum! Explicabo excepturi sed, mollitia illum exercitationem fuga dolores. </p>
-              <br />
-              <div class="text-small text-muted">
-                {" "}
-                <span className="code d-inline-block"></span> Language
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-6 pb-4 mb-2">
-          {" "}
-          <div className="border rounded p-3 pinned-container">
-            <div className="d-flex flex-row justify-content-between ">
-              <a href="#" className="h6">
-                project title
-              </a>
-              <span className="h6 text-muted text-small">
-                <small className="border rounded-pill p-1">Public </small>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-grid-3x3-gap mx-2" viewBox="0 0 16 16">
-                  <path d="M4 2v2H2V2h2zm1 12v-2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V7a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm5 10v-2a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V7a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V2a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zM9 2v2H7V2h2zm5 0v2h-2V2h2zM4 7v2H2V7h2zm5 0v2H7V7h2zm5 0h-2v2h2V7zM4 12v2H2v-2h2zm5 0v2H7v-2h2zm5 0v2h-2v-2h2zM12 1a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1h-2zm-1 6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V7zm1 4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1h-2z" />
-                </svg>
+                <small className="border rounded-pill py-1 px-2 ">Public </small>
+                <i class="fas fa-grip-vertical text-muted mx-2 px-1"></i>
               </span>
             </div>
             <div>
-              <p className="text-truncate text-small text-muted"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos optio sequi veritatis, velit quisquam, debitis similique itaque deleniti, adipisci eius corrupti eum! Explicabo excepturi sed, mollitia illum exercitationem fuga dolores. </p>
+              <p className="text-truncate* text-small text-muted">{repo.description?.substring(0, 130)}</p>
               <br />
-              <div class="text-small text-muted">
-                {" "}
-                <span className="code d-inline-block"></span> Language
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-6 pb-4 mb-2">
-          <div className="border rounded p-3 pinned-container">
-            <div className="d-flex flex-row justify-content-between ">
-              <a href="#" className="h6">
-                project title
-              </a>
-              <span className="h6 text-muted text-small">
-                <small className="border rounded-pill p-1">Public </small>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-grid-3x3-gap mx-2" viewBox="0 0 16 16">
-                  <path d="M4 2v2H2V2h2zm1 12v-2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V7a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm5 10v-2a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V7a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V2a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zM9 2v2H7V2h2zm5 0v2h-2V2h2zM4 7v2H2V7h2zm5 0v2H7V7h2zm5 0h-2v2h2V7zM4 12v2H2v-2h2zm5 0v2H7v-2h2zm5 0v2h-2v-2h2zM12 1a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1h-2zm-1 6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V7zm1 4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1h-2z" />
-                </svg>
-              </span>{" "}
-            </div>
-            <div>
-              <p className="text-truncate text-small text-muted"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos optio sequi veritatis, velit quisquam, debitis similique itaque deleniti, adipisci eius corrupti eum! Explicabo excepturi sed, mollitia illum exercitationem fuga dolores. </p>
-              <br />
-              <div class="text-small text-muted">
-                {" "}
-                <span className="code d-inline-block"></span> Language
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-6 pb-4 mb-2">
-          {" "}
-          <div className="border rounded p-3 pinned-container">
-            <div className="d-flex flex-row justify-content-between ">
-              <a href="#" className="h6">
-                project title
-              </a>
-              <span className="h6 text-muted text-small">
-                <small className="border rounded-pill p-1">Public </small>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-grid-3x3-gap mx-2" viewBox="0 0 16 16">
-                  <path d="M4 2v2H2V2h2zm1 12v-2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V7a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm5 10v-2a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V7a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V2a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zM9 2v2H7V2h2zm5 0v2h-2V2h2zM4 7v2H2V7h2zm5 0v2H7V7h2zm5 0h-2v2h2V7zM4 12v2H2v-2h2zm5 0v2H7v-2h2zm5 0v2h-2v-2h2zM12 1a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1h-2zm-1 6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V7zm1 4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1h-2z" />
-                </svg>
-              </span>{" "}
-            </div>
-            <div>
-              <p className="text-truncate text-small text-muted"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos optio sequi veritatis, velit quisquam, debitis similique itaque deleniti, adipisci eius corrupti eum! Explicabo excepturi sed, mollitia illum exercitationem fuga dolores. </p>
-              <br />
-              <div class="text-small text-muted">
-                {" "}
-                <span className="code d-inline-block"></span> Language
+              <div class="d-flex flex-row align-items-center gap-3 text-small text-muted position-absolute bottom-0 language">
+                <div>
+                  <span className={`code d-inline-block mx-1 ${repo.language.toLowerCase()}`}></span> <span>{repo.language}</span>
+                </div>
+                <div>
+                  <span>
+                    <i class="far fa-star mx-1"></i> {repo.stargazers_count}
+                  </span>
+                </div>
+
+                <div className={repo.forks === 0 ? "d-none" : "d-block"}>
+                  <span>
+                    <i class="fas fa-code-branch mx-1 "></i>
+                    {repo.forks}
+                  </span>
+                </div>
               </div>
             </div>
           </div>

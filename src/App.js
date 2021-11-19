37a6a5e446fafdc61a8b121d3d8f8ea93ae25a0f
Gustave Anthony Dzoumedor <dzoumedor13@gmail.com>
2021-11-19 02:31:06 +0000

    Added Loader

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ function App() {
   // const Github Repo
   // language;
   const [gitRepos, getRepo] = useState([]);
+  const [isloading, getIsLoading] = useState(true);
   useEffect(() => {
     Axios.get(`https://api.github.com/users/vanderkilu/repos?per_page=15&${process.env.REACT_APP_CLIENT_ID}&${process.env.REACT_APP_CLIENT_ID}`).then((response) => {
       //filter out repos who dont have a language and description
       let newRepo = [];
       (response.data || []).map((res) => {
         if (res.description && res.language && res.stargazers_count >= 0) {
-          return newRepo.push(res);
+          return newRepo.push(res), getIsLoading(false);
         }
       });
       getRepo(newRepo);
@@ -30,15 +31,27 @@ function App() {
           </side>
         </div>
         <div className="col-9">
-          <div className="d-flex flex-row justify-content-between align-items-center">
-            <p className="h6">Pinned</p>
-            <p className="customize text-muted text-decoration-none">Customize your pins</p>
-          </div>
-          <div className="row">
-            {gitRepos.map((repo) => {
-              return <PinnedRepo repo={repo} key={repo.id} />;
-            })}
-          </div>
+          {isloading && (
+            <div className="text-center pt-5 mt-5">
+              <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
+              <span class="visually-hidden*"> Loading Please wait ...</span>
+            </div>
+          )}
+
+          {!isloading && (
+            <div>
+              <div className="d-flex flex-row justify-content-between align-items-center">
+                <p className="h6 text-muted">Pinned</p>
+                <p className="customize text-muted text-decoration-none">Customize your pins</p>
+              </div>
+
+              <div className="row">
+                {gitRepos.map((repo) => {
+                  return <PinnedRepo repo={repo} key={repo.id} />;
+                })}
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>

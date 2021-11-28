e7cf23f4291366c23673fc61409fbc2d5e28e6be
Gustave Anthony Dzoumedor <dzoumedor13@gmail.com>
2021-11-28 17:37:36 +0000

    added ability to drag and change state plus minor fixes

--- src/Screens/PinnedRepos.js
+++ src/Screens/PinnedRepos.js
@@ -1,16 +1,34 @@
+import React, { useState, useEffect } from "react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import PinnedRepo from "./PinnedRepo";
 
 function PinnedRepos({ gitRepos }) {
+  const [repos, setNewRepo] = useState(gitRepos);
+
+  useEffect(() => {
+    setNewRepo(gitRepos);
+  }, [gitRepos]);
+
+  function handleOnDragEnd(pinned) {
+    if (!pinned.destination) return;
+
+    const items = Array.from(repos);
+    const [reorderedItem] = items.splice(pinned.source.index, 1);
+    items.splice(pinned.destination.index, 0, reorderedItem);
+    setNewRepo(items);
+  }
+
   return (
     <>
-      <DragDropContext>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        {" "}
         <Droppable droppableId="pinnedRepos">
           {(provided) => (
             <div className="row" {...provided.droppableProps} ref={provided.innerRef}>
-              {gitRepos.map((repo, index) => {
+              {repos.map((repo, index) => {
                 return <PinnedRepo repo={repo} key={`${repo.id}`} index={index} />;
               })}
+              {provided.placeholder}
             </div>
           )}
         </Droppable>
